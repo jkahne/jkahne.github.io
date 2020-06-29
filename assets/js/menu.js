a92fdb75d6dea31df1c7cb17e913490cb4931270
jkahne <jkahne@gmail.com>
2020-06-29 19:16:59 -0400

    more sassified

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,10 +1,10 @@
 const navSlide = () => {
-  const burger = document.querySelector('.burger');
+  const burger = document.querySelector('.nav_links__burger');
   const nav = document.querySelector('.nav_links');
 
   burger.addEventListener("click", () => {
     nav.classList.toggle('nav_links--active');
-    burger.classList.toggle("burger--active");
+    burger.classList.toggle("nav_links__burger--active");
   });
 }
 
